9dc8ec8c13d6fe1bc289248c2b5707eccef627b2
them0rg <45030326+them0rg@users.noreply.github.com>
2021-01-02 20:42:40 +0100

    adding basic command/listen

--- src/main/java/com/github/them0rg/discosnek/discosnek.java
+++ src/main/java/com/github/them0rg/discosnek/discosnek.java
@@ -1,15 +1,40 @@
 package com.github.them0rg.discosnek;
 
-import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+interface Command {
+    Mono<Void> execute(MessageCreateEvent event);
+}
 
 public class discosnek {
 
+    private static final Map<String, Command> commands = new HashMap<>();
+
+    static {
+        commands.put("ping", event -> event.getMessage().getChannel()
+                .flatMap(channel -> channel.createMessage("Pong!"))
+                .then());
+    }
+
     public static void main(String[] args) {
         final GatewayDiscordClient client = DiscordClientBuilder.create(args[0]).build()
-            .login()
-            .block();
+                .login()
+                .block();
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .flatMap(event -> Mono.just(event.getMessage().getContent())
+                        .flatMap(content -> Flux.fromIterable(commands.entrySet())
+                                .filter(entry -> content.startsWith('!' + entry.getKey()))
+                                .flatMap(entry -> entry.getValue().execute(event))
+                                .next()))
+                .subscribe();
         client.onDisconnect().block();
+
     }
 }
