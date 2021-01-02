2476949d2feaf80cb9e7175401269939d697b037
them0rg <45030326+them0rg@users.noreply.github.com>
2021-01-02 21:31:30 +0100

    adding broken track func

--- src/main/java/com/github/them0rg/discosnek/discosnek.java
+++ src/main/java/com/github/them0rg/discosnek/discosnek.java
@@ -1,11 +1,20 @@
 package com.github.them0rg.discosnek;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.Member;
+import discord4j.voice.AudioProvider;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,13 +24,30 @@ interface Command {
 
 public class discosnek {
 
-    private static final Map<String, Command> commands = new HashMap<>();
-
     static {
         commands.put("ping", event -> event.getMessage().getChannel()
                 .flatMap(channel -> channel.createMessage("Pong!"))
                 .then());
+        commands.put("join", event -> Mono.justOrEmpty(event.getMember())
+                .flatMap(Member::getVoiceState)
+                .flatMap(VoiceState::getChannel)
+                .flatMap(channel -> channel.join(spec -> spec.setProvider(provider)))
+                .then());
+        final TrackScheduler scheduler = new TrackScheduler(player);
+        commands.put("play", event -> Mono.justOrEmpty(event.getMessage().getContent())
+                .map(content -> Arrays.asList(content.split(" ")))
+                .doOnNext(command -> playerManager.loadItem(command.get(1), scheduler))
+                .then());
     }
+    playerManager.getConfiguration().
+    final AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
+    AudioSourceManagers.registerRemoteSources(playerManager);
+    final AudioPlayer player = playerManager.createPlayer();
+    AudioProvider provider = new LavaPlayerAudioProvider(player);
+
+    private static final Map<String, Command> commands = new HashMap<>();
+
+    setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
 
     public static void main(String[] args) {
         final GatewayDiscordClient client = DiscordClientBuilder.create(args[0]).build()
